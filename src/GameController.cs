5ca984aab419ab022258690077d7bb08b76fd0e7
bryanhon98 <48546959+bryanhon98@users.noreply.github.com>
2019-04-24 21:06:15 +0800

    last changes made

--- src/GameController.cs
+++ src/GameController.cs
@@ -6,6 +6,14 @@ using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
 using SwinGameSDK;
+using static GameController;
+using static UtilityFunctions;
+using static GameResources;
+using static DeploymentController;
+using static DiscoveryController;
+using static EndingGameController;
+using static MenuController;
+using static HighScoreController;
 
 /// <summary>
 /// The GameController is responsible for controlling the game,
