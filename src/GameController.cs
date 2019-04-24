c7e05b00099c65f5260ac0b014131b19747bd289
bryanhon98 <48546959+bryanhon98@users.noreply.github.com>
2019-04-25 00:01:37 +0800

    changes made again

--- src/GameController.cs
+++ src/GameController.cs
@@ -84,6 +84,9 @@ public static class GameController
 
 		//create the players
 		switch (_aiSetting) {
+			case AIOption.Easy:
+			_ai = new AIEasyPlayer(_theGame);
+			break;
 			case AIOption.Medium:
 				_ai = new AIMediumPlayer(_theGame);
 				break;
@@ -91,7 +94,7 @@ public static class GameController
 				_ai = new AIHardPlayer(_theGame);
 				break;
 			default:
-				_ai = new AIHardPlayer(_theGame);
+				_ai = new AIEasyPlayer(_theGame);
 				break;
 		}
 
