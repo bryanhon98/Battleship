cbcb8f5bc8dd1d83b32239e1a6b1d8d667bc2aeb
bryanhon98 <48546959+bryanhon98@users.noreply.github.com>
2019-05-23 13:34:02 +0800

    last change

---
+++ bin/Debug/Unit Testing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SwinGameSDK;
+using NUnit.Framework;
+
+namespace MyGame
+{
+	[TestFixture ()]
+	public class Unit_Testing
+	{
+		[Test ()]
+		public void TestSelectedBS ()
+		{
+			ShipName selectedBS = ShipName.Tug;
+			ShipName select = default (ShipName);
+
+			if (select == ShipName.None) {
+				selectedBS = select;
+			}
+
+			Assert.AreEqual (select, selectedBS);
+		}
+	}
+}
+
+
